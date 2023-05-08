18676b85d51056c1d68ecd2f924aca452a4c421c
KshitijBasve <kshitijbasve@gmail.com>
2023-05-08 15:52:21 +0530

    Added CSS and JavaScript Files

---
+++ index.js
@@ -0,0 +1,50 @@
+// var state = {
+//     taskList: [
+//         {
+//             imageurl: "",
+//             taskTitle: "",
+//             taskType: "",
+//             taskDescription: "",
+//         },
+
+//         {
+//             imageurl: "",
+//             taskTitle: "",
+//             taskType: "",
+//             taskDescription: "",
+//         },
+
+//         {
+//             imageurl: "",
+//             taskTitle: "",
+//             taskType: "",
+//             taskDescription: "",
+//         },
+
+//         {
+//             imageurl: "",
+//             taskTitle: "",
+//             taskType: "",
+//             taskDescription: "",
+//         },
+
+//         {
+//             imageurl: "",
+//             taskTitle: "",
+//             taskType: "",
+//             taskDescription: "",
+//         },
+//     ],
+// }
+
+const state = {
+    taskList: [],
+};
+
+// DOM Operations
+
+const taskcontents = document.querySelector(".task__contents");
+const taskcModal = document.querySelector(".task__Modal__body");
+
+console.log(taskcontents);
+console.log(taskModal);
